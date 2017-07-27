8249925aa73a396bc93ff185dd9e4d0bf40deab0
Daniele <daniele.germani.85@gmail.com>
2017-07-27 09:23:39 +0200

    iteration 1 ok

--- starter-code/src/app/services/cinema-service.service.ts
+++ starter-code/src/app/services/cinema-service.service.ts
@@ -1,50 +1,24 @@
 import { Injectable } from '@angular/core';
-// import * as _ from 'lodash';
-import * as _ from 'underscore';
 
-const moviesList = require('./sample-movies');
-class Movie {
-  constructor(public id: number, public title: string, public poster: string,
-              public synopsis: string, public genres: Array<string>,public year: number,
-              public director: string, public actors: Array<string>,
-              public hours: string, public room: number
-            ){
-            }
+
+const moviesList = require('../../sample-movies');
+export class Movie {
+  constructor(public id:number, public title:string, public poster: string, public synopsis:string, public genres:Array<string>, public year:number, public director:string, public actors:Array<string>, hours: Array<string>, room:number) {}
 }
-
 
 @Injectable()
 export class CinemaServiceService {
+    movies: Array<Movie> = moviesList;
 
-  rooms = [1, 2, 3, 4, 5];
-  hours = ['18:20', '21:20', '22:30', '23:30'];
-  days = ['Mon', 'Tue', 'Wed', 'Sat', 'Sun'];
+    constructor() { }
 
-  movies: Movie[] = [];
-  constructor() {
-    this.addMoviesList();
-  }
+    getMovies() {
+      return this.movies;
+    }
 
-  addMoviesList() {
-    moviesList.forEach((e, i) => {
-      let roomN = _.sample(this.rooms);
-      let hour = _.sample(this.hours,2);
-      let day = _.sample(this.days);
-
-      this.movies.push(new Movie(e.id, e.title, e.poster,  e.synopsis,
-                                e.genres, e.year,  e.director,
-                                e.actors, `${day} at ${hour[0]} and ${hour[1]}`, roomN[0]));
-
-    })
-  }
-
-  getMovies() {
-    return this.movies;
-  }
-  getMovie(id: number) {
-    this.movies.forEach(e => {
-      if (e.id == id) return e;
-    })
-    return false;
-  }
+    getMovie(id:number) {
+      this.movies.forEach( movie => {
+        if(movie.id === id) return movie;
+      });
+    }
 }
